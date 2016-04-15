be63a3f90cad56de146e5fdca0500283aa9329a2
noe stauffert <noe.stauffert@gmail.com>
2016-04-15 15:00:59 -0700

    added auto-selection for generated short url

--- app/assets/javascripts/urls.js
+++ app/assets/javascripts/urls.js
@@ -1,2 +1,14 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).ready(function() {
+
+  var range = document.createRange();
+  var selection = window.getSelection();
+  range.selectNodeContents(document.getElementById('short-url'));
+
+  selection.removeAllRanges();
+  selection.addRange(range);
+});
+
+
