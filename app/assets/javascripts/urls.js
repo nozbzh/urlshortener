61ece57e3dab552fb6971bb9fcdf06280b2d30ac
noe stauffert <noe.stauffert@gmail.com>
2016-04-15 15:53:56 -0700

    commented all JS to check if chrome warning still shows in production

--- app/assets/javascripts/urls.js
+++ app/assets/javascripts/urls.js
@@ -1,37 +1,37 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+// // Place all the behaviors and hooks related to the matching controller here.
+// // All this logic will automatically be available in application.js.
 
-$(document).ready(function() {
+// $(document).ready(function() {
 
-  $('#short-url').popover('show');
+//   $('#short-url').popover('show');
 
-  // var range = document.createRange();
-  // var selection = window.getSelection();
-  // range.selectNodeContents(document.getElementById('short-url'));
+//   // var range = document.createRange();
+//   // var selection = window.getSelection();
+//   // range.selectNodeContents(document.getElementById('short-url'));
 
-  // selection.removeAllRanges();
-  // selection.addRange(range);
+//   // selection.removeAllRanges();
+//   // selection.addRange(range);
 
-  function SelectText(element) {
-    var doc = document
-        , text = doc.getElementById(element)
-        , range, selection
-    ;
-    if (doc.body.createTextRange) {
-        range = document.body.createTextRange();
-        range.moveToElementText(text);
-        range.select();
-    } else if (window.getSelection) {
-        selection = window.getSelection();
-        range = document.createRange();
-        range.selectNodeContents(text);
-        selection.removeAllRanges();
-        selection.addRange(range);
-    }
-  }
+//   function SelectText(element) {
+//     var doc = document
+//         , text = doc.getElementById(element)
+//         , range, selection
+//     ;
+//     if (doc.body.createTextRange) {
+//         range = document.body.createTextRange();
+//         range.moveToElementText(text);
+//         range.select();
+//     } else if (window.getSelection) {
+//         selection = window.getSelection();
+//         range = document.createRange();
+//         range.selectNodeContents(text);
+//         selection.removeAllRanges();
+//         selection.addRange(range);
+//     }
+//   }
 
-  SelectText('short-url');
+//   SelectText('short-url');
 
-});
+// });
 
 
