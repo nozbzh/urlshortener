07654b2d8493500dd773cc06dae83d77567c3994
noe stauffert <noe.stauffert@gmail.com>
2016-04-15 15:51:11 -0700

    added function to JS autoselect

--- app/assets/javascripts/urls.js
+++ app/assets/javascripts/urls.js
@@ -5,12 +5,33 @@ $(document).ready(function() {
 
   $('#short-url').popover('show');
 
-  var range = document.createRange();
-  var selection = window.getSelection();
-  range.selectNodeContents(document.getElementById('short-url'));
+  // var range = document.createRange();
+  // var selection = window.getSelection();
+  // range.selectNodeContents(document.getElementById('short-url'));
 
-  selection.removeAllRanges();
-  selection.addRange(range);
+  // selection.removeAllRanges();
+  // selection.addRange(range);
+
+  function SelectText(element) {
+    var doc = document
+        , text = doc.getElementById(element)
+        , range, selection
+    ;
+    if (doc.body.createTextRange) {
+        range = document.body.createTextRange();
+        range.moveToElementText(text);
+        range.select();
+    } else if (window.getSelection) {
+        selection = window.getSelection();
+        range = document.createRange();
+        range.selectNodeContents(text);
+        selection.removeAllRanges();
+        selection.addRange(range);
+    }
+  }
+
+  SelectText('short-url');
+
 });
 
 
