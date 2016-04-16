a5e06571c1d1fe7d55888722c7aa4cabca3818fd
noe stauffert <noe.stauffert@gmail.com>
2016-04-15 21:07:12 -0700

    changed Urls #edit to debug production ...

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -25,10 +25,9 @@ class UrlsController < ApplicationController
 
   def edit
     page = (1..10).to_a.sample
-    results = GoogleCustomSearchApi.search('komodo', page: page, searchType: 'image')
+    @results = GoogleCustomSearchApi.search('komodo', page: page, searchType: 'image')
     result = (0..9).to_a.sample
-    @full_result = results["items"][result]
-    # @image = @full_result["link"]
+    # @image = results["items"][result]["link"]
     @url = Url.find(params[:id])
   end
 
