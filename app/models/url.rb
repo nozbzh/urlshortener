821c6bfb8117a18724ddcbf116ef842146ada6b5
noe stauffert <noe.stauffert@gmail.com>
2016-05-12 13:08:55 -0700

    added '!' to url regex

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,7 +1,7 @@
 class Url < ApplicationRecord
   validates :original_url, presence: true, on: :create
   validates_format_of :original_url,
-    with: /\A(?:(?:http|https):\/\/)?([-a-zA-Z0-9.]{2,256}\.[a-z]{2,4})\b(?:\/[-a-zA-Z0-9@,:%_\+.~#?&\/\/=]*)?\z/
+    with: /\A(?:(?:http|https):\/\/)?([-a-zA-Z0-9.]{2,256}\.[a-z]{2,4})\b(?:\/[-a-zA-Z0-9@,!:%_\+.~#?&\/\/=]*)?\z/
   before_create :generate_short_url
 
   def generate_short_url
